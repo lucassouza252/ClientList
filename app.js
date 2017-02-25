90f9f5c1e16bbb8c172feda1df38f26e61b71654
Lucas Souza <lucas252souza@gmail.com>
2017-02-25 13:15:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -15,18 +15,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/listApp'));
-app.use(cors({origin: '*'}));
+//app.use(cors({origin: '*'}));
 app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "http://lcteste.esy.es");
-    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Authorization, Content-Type, X-Requested-With");
     res.header("Access-Control-Allow-Methods", "GET, POST");
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
 app.all("/api/*", function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://lcteste.esy.es");
-    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
     res.setHeader('Access-Control-Allow-Credentials', true);
     return next();
